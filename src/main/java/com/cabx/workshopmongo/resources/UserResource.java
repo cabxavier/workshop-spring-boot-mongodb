d6f3b1f9f02c2cd73dbe265db7edecd34c3d7bd9
César Augusto Barbosa Xavier <cesaraugustobarbosaxavier@gmail.com>
2022-10-12 15:26:26 -0300

    Obtendo um usuário por id

--- src/main/java/com/cabx/workshopmongo/resources/UserResource.java
+++ src/main/java/com/cabx/workshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,23 +15,29 @@ import com.cabx.workshopmongo.dto.UserDTO;
 import com.cabx.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<UserDTO>> findAll(){
-		
+	public ResponseEntity<List<UserDTO>> findAll() {
+
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = new ArrayList<>();
-		
+
 		for (int i = 0; i < list.size(); i++) {
-			
+
 			listDto.add(new UserDTO(list.get(i)));
 		}
-		
+
 		return ResponseEntity.ok().body(listDto);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+
+		return ResponseEntity.ok().body(new UserDTO(service.findById(id)));
 	}
 }
