41ce2f41c18e193ee703ceea966555968e5b39b7
César Augusto Barbosa Xavier <cesaraugustobarbosaxavier@gmail.com>
2022-10-12 12:40:15 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/cabx/workshopmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.cabx.workshopmongo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.cabx.workshopmongo.domain.User;
+import com.cabx.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Browns", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.save(maria);
+		userRepository.save(alex);
+		userRepository.save(bob);
+	}
+
+}
