59075100b015d0c96e8c386a98eb69ca98abd299
César Augusto Barbosa Xavier <cesaraugustobarbosaxavier@gmail.com>
2022-10-16 15:13:04 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/cabx/workshopmongo/config/Instantiation.java
+++ src/main/java/com/cabx/workshopmongo/config/Instantiation.java
@@ -1,30 +1,45 @@
 package com.cabx.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.TimeZone;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.cabx.workshopmongo.domain.Post;
 import com.cabx.workshopmongo.domain.User;
+import com.cabx.workshopmongo.repository.PostRepository;
 import com.cabx.workshopmongo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
-	
+
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private PostRepository postRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
 		userRepository.deleteAll();
-		
+		postRepository.deleteAll();
+
 		User maria = new User(null, "Maria Browns", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		userRepository.save(maria);
-		userRepository.save(alex);
-		userRepository.save(bob);
-	}
 
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+				maria);
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+	}
 }
